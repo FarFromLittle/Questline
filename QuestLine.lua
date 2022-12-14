4ccd6bb2b84aeb40daf8fdc0b731a9b5782074ea
FarFromLittle <mail.winston@yahoo.com>
2022-12-14 00:10:43 -0600

    Update QuestLine.lua

--- QuestLine.lua
+++ QuestLine.lua
@@ -1,18 +1,115 @@
---[[ Authored by FarFromLittle ]]--
+--!strict
 
-local QuestLine = {}
+-- Authored by FarFromLittle
+
+local Objective = {}
+
+function Objective.event(event:RBXScriptSignal, count:number)
+	local function objective(player:Player, progress:number)
+		local plr
+		
+		for i = 1, count - progress do
+			repeat plr = coroutine.yield(progress, event)
+			until plr == player
+			progress += 1
+		end
+		
+		return progress
+	end
+	
+	return objective, count
+end
+
+function Objective.score(name:string, amount:number)
+	local function objective(player:Player, progress:number)
+		local ldr = player:FindFirstChild("leaderstats")
+		while not(ldr and ldr.Name == "leaderstats") do ldr = coroutine.yield(progress, player.ChildAdded) end
+		
+		local int = ldr:FindFirstChild(name)::IntValue
+		while not(int and int.Name == name) do int = coroutine.yield(progress, ldr.ChildAdded) end
+		
+		if amount <= int.Value then return amount end
+		
+		progress = int.Value
+		
+		while progress < amount do progress = coroutine.yield(progress, int.Changed) end
+		
+		return amount
+	end
+	
+	return objective, amount
+end
+
+function Objective.timer(sec:number, one:boolean?)
+	local function objective(player:Player, progress:number)
+		local event = Instance.new("BindableEvent")
+		
+		while progress < sec do
+			task.delay(1, event.Fire, event, progress)
+			
+			if one then
+				coroutine.yield(0, event.Event)
+			else
+				progress = coroutine.yield(progress, event.Event)
+			end
+			
+			progress += 1
+		end
+		
+		event:Destroy()
+		
+		return one and 1 or progress
+	end
+	
+	return objective, one and 1 or sec
+end
+
+function Objective.touch(touchPart:BasePart)
+	local function objective(player:Player, progress:number)
+		local hit
+		
+		repeat
+			hit = coroutine.yield(progress, touchPart.Touched)
+		until hit.Parent == player.Character
+		
+		return 1
+	end
+	
+	return objective, 1
+end
+
+function Objective.value(intValue:IntValue, amount:number)
+	local function objective(player:Player, progress:number)
+		while progress < amount do
+			progress = coroutine.yield(progress, intValue.Changed)
+		end
+		
+		return amount
+	end
+	
+	return objective, amount
+end
+
+
+local QuestLine = {
+	Event = "event",
+	Score = "score",
+	Timer = "timer",
+	Touch = "touch",
+	Value = "value"
+}
 QuestLine.__index = QuestLine
 
 export type QuestLine = typeof(QuestLine)
-export type Objective = (player:Player, progress:number)->(number, RBXScriptSignal?)
+export type Objective = "event"|"score"|"timer"|"touch"|"value"
 
 QuestLine.timeout = 1
 
-function QuestLine:Accepted(player:Player) end
-function QuestLine:Assigned(player:Player, progress:number, index:number) end
-function QuestLine:Canceled(player:Player) end
-function QuestLine:Completed(player:Player) end
-function QuestLine:Progressed(player:Player, progress:number, index:number) end
+function QuestLine:OnAccept(player:Player) end
+function QuestLine:OnAssign(player:Player, progress:number, index:number) end
+function QuestLine:OnCancel(player:Player) end
+function QuestLine:OnComplete(player:Player) end
+function QuestLine:OnProgress(player:Player, progress:number, index:number) end
 
 local questIndex:{[QuestLine]:string} = {}
 local questTable:{[string]:QuestLine} = {}
@@ -22,7 +119,7 @@ local playerObjective:{[Player]:{[QuestLine]:thread}} = {}
 local playerProgress:{[Player]:{[string]:number}} = {}
 
 local objectiveCount:{[QuestLine]:number} = {}
-local objectiveTable:{[QuestLine]:{Objective}} = {}
+local objectiveTable:{[QuestLine]:{(plr:Player, prg:number)->(number, RBXScriptSignal?)}} = {}
 local objectiveValue:{[QuestLine]:{number}} = {}
 
 local Error = {
@@ -77,8 +174,8 @@ function QuestLine.getQuestById(questId:string):QuestLine
 	return questTable[tostring(questId):sub(1, 50)]
 end
 
-function QuestLine.new(questId:string, obj:{any}?):QuestLine
-	local self = setmetatable(obj or {}, QuestLine)
+function QuestLine.new(questId:string, obj:{any}?)
+	local self = setmetatable(obj or {}, QuestLine)::QuestLine
 	
 	questId = tostring(questId):sub(1, 50)
 	
@@ -130,38 +227,38 @@ function QuestLine:Assign(player:Player)
 		
 		playerProgress[player][tostring(self)] = 0
 		
-		self:Accepted(player)
+		self:OnAccept(player)
 	else
 		progress = self:GetProgress(player)
 		currentProgress, index = self:GetCurrentProgress(player)
 	end
 	
-	local objective = objectiveTable[self][index]
+	local coro = objectiveTable[self][index]
 	
-	self:Assigned(player, currentProgress, index)
-	self:Progressed(player, currentProgress, index)
+	self:OnAssign(player, currentProgress, index)
+	self:OnProgress(player, currentProgress, index)
 	
 	local conn
 	local event = empty
-	local obj = coroutine.create(objective or dead)
+	local obj = coroutine.create(coro or dead)
 	
 	local function connect(...)
 		if coroutine.status(obj) == "dead" then
 			index += 1
 			currentProgress = 0
-			objective = objectiveTable[self][index]
+			coro = objectiveTable[self][index]
 			
-			if not objective then
+			if not coro then
 				disconnect(player, self)
 				
-				return self:Completed(player)
+				return self:OnComplete(player)
 			end
 			
 			event = empty
-			obj = coroutine.create(objective)
+			obj = coroutine.create(coro)
 			playerObjective[player][self] = obj
 			
-			self:Progressed(player, currentProgress, index)
+			self:OnProgress(player, currentProgress, index)
 		end
 		
 		local ok, prg, evt = coroutine.resume(obj, ...)
@@ -175,7 +272,7 @@ function QuestLine:Assign(player:Player)
 			
 			currentProgress = prg
 			
-			self:Progressed(player, currentProgress, index)
+			self:OnProgress(player, currentProgress, index)
 		end
 		
 		if event == evt then return end
@@ -201,7 +298,7 @@ function QuestLine:Cancel(player:Player)
 	
 	playerProgress[player][questIndex[self]] = -(progress + 1)
 	
-	self:Canceled(player)
+	self:OnCancel(player)
 end
 
 function QuestLine:GetProgress(player:Player)
@@ -210,8 +307,8 @@ function QuestLine:GetProgress(player:Player)
 	return playerProgress[player][tostring(self)] or 0
 end
 
-function QuestLine:GetCurrentProgress(player:Player):(number?, number?)
-	if not self:IsAccepted(player) or self:IsComplete(player) then return end
+function QuestLine:GetCurrentProgress(player:Player):(number, number)
+	if not self:IsAccepted(player) or self:IsComplete(player) then return 0, 0 end
 	
 	local progress = self:GetProgress(player)
 	
@@ -222,7 +319,7 @@ function QuestLine:GetCurrentProgress(player:Player):(number?, number?)
 	local objectives = objectiveTable[self]
 	local values = objectiveValue[self]
 	
-	if not objectives then return end
+	if not objectives then return 0, 0 end
 	
 	local currentProgress = progress
 	local index = 1
@@ -235,10 +332,10 @@ function QuestLine:GetCurrentProgress(player:Player):(number?, number?)
 	return currentProgress, index
 end
 
-function QuestLine:AddObjective(obj:Objective, val:number?):number
-	local index = #objectiveTable[self] + 1
+function QuestLine:AddObjective(objType:Objective, ...:any):number
+	local obj, val = Objective[objType](...)
 	
-	if not val then val = 1 end
+	local index = #objectiveTable[self] + 1
 	
 	objectiveCount[self] += val
 	objectiveTable[self][index] = obj
@@ -249,94 +346,6 @@ end
 
 function QuestLine:GetObjectiveValue(index:number):number
 	return objectiveValue[self][index]
-end
-
--- OBJECTIVES
-
-function QuestLine.Event(event:RBXScriptSignal, count:number)
-	local function objective(player:Player, progress:number)
-		local plr
-		
-		for i = 1, count - progress do
-			repeat plr = coroutine.yield(progress, event)
-			until plr == player
-			progress += 1
-		end
-		
-		return progress
-	end
-	
-	return objective, count
-end
-
-function QuestLine.Score(name:string, amount:number)
-	local function objective(player:Player, progress:number)
-		local ldr = player:FindFirstChild("leaderstats")
-		while not(ldr and ldr.Name == "leaderstats") do ldr = coroutine.yield(progress, player.ChildAdded) end
-		
-		local int = ldr:FindFirstChild(name)
-		while not(int and int.Name == name) do int = coroutine.yield(progress, ldr.ChildAdded) end
-		
-		if amount <= int.Value then return amount end
-		
-		progress = int.Value
-		
-		while progress < amount do progress = coroutine.yield(progress, int.Changed) end
-		
-		return amount
-	end
-	
-	return objective, amount
-end
-
-function QuestLine.Timer(sec:number, one:boolean?)
-	local function objective(player:Player, progress:number)
-		local event = Instance.new("BindableEvent")
-		
-		while progress < sec do
-			task.delay(1, event.Fire, event, progress)
-			
-			if one then
-				coroutine.yield(0, event.Event)
-			else
-				progress = coroutine.yield(progress, event.Event)
-			end
-			
-			progress += 1
-		end
-		
-		event:Destroy()
-		
-		return one and 1 or progress
-	end
-	
-	return objective, sec
-end
-
-function QuestLine.Touch(touchPart:BasePart)
-	local function objective(player:Player, progress:number)
-		local hit
-		
-		repeat
-			hit = coroutine.yield(progress, touchPart.Touched)
-		until hit.Parent == player.Character
-		
-		return 1
-	end
-	
-	return objective, 1
-end
-
-function QuestLine.Value(intValue:string, amount:number)
-	local function objective(player:Player, progress:number)
-		while progress < amount do
-			progress = coroutine.yield(progress, intValue.Changed)
-		end
-		
-		return amount
-	end
-	
-	return objective, amount
 end
 
 return QuestLine
